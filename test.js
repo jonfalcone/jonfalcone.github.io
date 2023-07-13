e8cf957b0d03b9179e8f0d85538d88cb35ebd0b9
Jon Falcone <jon.falcone@spotim1.onmicrosoft.com>
2023-07-13 16:47:11 -0700

    updating test script to try to see if params work

--- test.js
+++ test.js
@@ -1,1 +1,8 @@
-(function (utmSource) {    localStorage.clear();    _WF.cache.setItem("logging", "*").asPersistent();    document.location =        document.location.protocol +        "//" +        document.location.host +        document.location.pathname +        `?utm_source=${utmSource}&utm_campaign=qa&utm_content=qa&utm_medium=qa&csplit=header&spallcontent=true&postcontent_ab=true`;})("ft");
+(function (utmSource) { 
+    chrome.storage.local.get(["param1"]).then((result) => {
+        utmSource = result.key;
+      });
+    localStorage.clear(); 
+    _WF.cache.setItem("logging", "*").asPersistent(); 
+    document.location = document.location.protocol + "//" + document.location.host + document.location.pathname + `?utm_source=${utmSource}&utm_campaign=qa&utm_content=qa&utm_medium=qa&csplit=header&spallcontent=true&postcontent_ab=true`; 
+})(utmSource);
