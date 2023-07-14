fab581bd62c701ec39b7e10051a26882c743c022
Jon Falcone <jon.falcone@spotim1.onmicrosoft.com>
2023-07-14 13:19:48 -0700

    Another oops

--- test.js
+++ test.js
@@ -1,5 +1,5 @@
-(function () { 
+(function (utmSource) { 
     localStorage.clear(); 
     _WF.cache.setItem("logging", "*").asPersistent(); 
-    document.location = document.location.protocol + "//" + document.location.host + document.location.pathname + `?utm_source=${source}&utm_campaign=qa&utm_content=qa&utm_medium=qa&csplit=header&spallcontent=true&postcontent_ab=true`; 
+    document.location = document.location.protocol + "//" + document.location.host + document.location.pathname + `?utm_source=${utmSource}&utm_campaign=qa&utm_content=qa&utm_medium=qa&csplit=header&spallcontent=true&postcontent_ab=true`; 
 })(localStorage.getItem("param1"));
