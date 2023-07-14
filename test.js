bde830415d49a22b2ee95f9cd2cd2cc2074f7ee1
Jon Falcone <jon.falcone@spotim1.onmicrosoft.com>
2023-07-13 17:04:24 -0700

    try localStorage

--- test.js
+++ test.js
@@ -1,9 +1,5 @@
-let utm_source;
-chrome.storage.local.get(["param1"]).then((result) => {
-    utmSource = result.key;
-});
-(function (source) { 
+(function () { 
     localStorage.clear(); 
     _WF.cache.setItem("logging", "*").asPersistent(); 
     document.location = document.location.protocol + "//" + document.location.host + document.location.pathname + `?utm_source=${source}&utm_campaign=qa&utm_content=qa&utm_medium=qa&csplit=header&spallcontent=true&postcontent_ab=true`; 
-})(utmSource);
+})(localStorage.getItem("param1"));
