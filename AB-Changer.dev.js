a0a077dd73d30033b7e10f7b21342cc2f3c278c8
jonfalcone <117866761+jonfalcone@users.noreply.github.com>
2023-12-05 11:58:12 -0800

    Create AB-Changer.dev.js

---
+++ AB-Changer.dev.js
@@ -0,0 +1,53 @@
+//Set/Modify AB tests that do not exist in the AB portal
+function MyPrompt() {
+    return new Promise((resolve, reject) => {
+        let dialog = document.createElement("dialog");
+        dialog.setAttribute("id", "dialog");
+        dialog.innerHTML = `
+          <form>
+            <label for="ptext">AB Test Name:</label>
+            <input class="text_input" type="text" required value="${localStorage.abDebug ? localStorage.abDebug : ''}" onclick="this.select()"/>
+            <br>
+            <label for="ptext">AB Test Value(s):</label>
+            <input class="text_input" type="text"/>
+            <br>
+            <button type="submit">Save</button>
+            <button style="margin-left:10px"; type="button" onclick="(function(){document.getElementById('dialog').remove();})();")>Exit</button>
+          </form>`;
+        document.body.appendChild(dialog);
+        dialog.showModal();
+        dialog.querySelector("form").addEventListener("submit", (e) => {
+            e.preventDefault();
+            dialog.remove();
+            var inputs = dialog.getElementsByClassName("text_input"),
+                names = [].map.call(inputs, function(input) {
+                    return input.value;
+                });
+            resolve(names);
+        });
+    });
+}
+(async function() {
+    const currentScript = document.currentScript;
+    const params = currentScript.getAttribute('params');
+    const attrNames = currentScript.getAttributeNames();
+    var newAB;
+
+    if(!params)
+    {
+        newAB = await MyPrompt();
+    }
+    else
+    {
+        newAB = params;
+    }
+
+    var setAB = function(values) {
+        localStorage.abDebug = values[0];
+        _WF.cache
+            .setItem(_WF.KEYS.TAGMAN_PREFIX + values[0], values[1], true)
+            .asPersistent();
+        location.reload();
+    };
+    setAB(newAB);
+})();
